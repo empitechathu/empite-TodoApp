145fedb8edaf7c34db1256c5b7f6d4ec14f124fb
empiteChathu <Chathuranga@empite.com>
2018-08-02 12:34:13 +0530

    initial commit with base template added

---
+++ EmpiteTodo/index.js
@@ -0,0 +1,6 @@
+import { AppRegistry } from 'react-native';
+import App from 'EmpiteTodo/App';
+import {name as appName} from 'EmpiteTodo/app.json';
+
+
+AppRegistry.registerComponent(appName, () => App);
